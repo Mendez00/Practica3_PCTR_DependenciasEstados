6cbefe44c6cb0b8153f7ec1079f6e4986b1f42f1
Álvaro Méndez <xma1001@alu.ubu.es>
2022-03-31 21:28:41 +0200

    Modificaciones SistemaLanzador

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -1,25 +1,25 @@
 package src.p03.c01;
 
 public class SistemaLanzador {
-	public static void main(String[] args) {
-		
-		IParque parque = new Parque(); // TODO
-		char letra_puerta = 'A';
-		
-		System.out.println("Â¡Parque abierto!");
-		
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
-			
-			String puerta = ""+((char) (letra_puerta++));
-			
-			// CreaciÃ³n de hilos de entrada
-			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
-			new Thread (entradas).start();
-			
-			// 
-			// TODO
-			//
-			
-		}
-	}	
+    public static void main(String[] args) {
+
+        IParque parque = new Parque(50);
+        char letra_puerta = 'A';
+        int numPuertas = args.length > 0 ? Integer.parseInt(args[0]) : 5;
+
+        System.out.println("¡Parque abierto!");
+
+        for (int i = 0; i < numPuertas; i++) {
+
+            String puerta = ""+((char) (letra_puerta++));
+
+            // Creación de hilos de entrada
+            ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
+            new Thread (entradas).start();
+
+            ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
+            new Thread(salidas).start();
+
+        }
+    }
 }
